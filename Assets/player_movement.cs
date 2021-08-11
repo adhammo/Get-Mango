718a652ebf17a60f50819423f2d23256e5d86833
Omar Khaled <omarelgammal.ok@gmail.com>
2021-08-11 19:12:35 +0200

    added animation

--- Assets/player_movement.cs
+++ Assets/player_movement.cs
@@ -7,16 +7,19 @@ public class player_movement : MonoBehaviour
     public CharacterController2D Contoller;
     public PlayerCharacter character;
     bool jump = false;
-
+    public Animator animator;
     // Update is called once per frame
     void Update()
     {
-        character.SetState(Input.GetAxisRaw("Horizontal"), jump);
 
+        character.SetState(Input.GetAxisRaw("Horizontal"), jump);
+        animator.SetFloat("speed", Mathf.Abs(Input.GetAxisRaw("Horizontal")));
         if (Input.GetKey("up")|| Input.GetAxisRaw("Jump")==1|| Input.GetAxisRaw("Vertical") == 1)
         {
             jump = true;
         }
         else jump = false;
+        animator.SetBool("isjumping", jump);
+        animator.SetFloat("jumping", Contoller.Velocity.y);
     }
 }
